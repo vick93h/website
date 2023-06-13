114431a178052af95cc22e5e4948ae108757c7f3
vick93h <vittoriogerry@gmail.com>
2023-06-13 18:06:57 +0200

    fix varie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WebSite';
+export class AppComponent implements OnInit,AfterViewInit{
+  @ViewChild('login') loginRef!:ElementRef;
+
+  ngOnInit(): void {
+
+  }
+
+  openLogin() {
+    if(this.loginRef.nativeElement.style.display=='block') {
+      this.loginRef.nativeElement.style.display = 'none'
+    }
+    else {
+      this.loginRef.nativeElement.style.display = 'block'
+    }
+  }
+
+  ngAfterViewInit(): void {/*
+    console.log("afterinit");
+    setTimeout(() => {
+      console.log(this.loginRef.nativeElement.innerText);
+    }, 1000);*/
+    this.loginRef.nativeElement.style.display='none'
+  }
 }
